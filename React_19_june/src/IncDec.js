53974129a3c1c6658e242b04481cdb119cff219f
Minhaj Haider <minhajhaider007@gmail.com>
2024-06-20 14:04:19 +0530

    reviseCode

--- React_19_june/src/IncDec.js
+++ React_19_june/src/IncDec.js
@@ -3,21 +3,27 @@ import React, { useState } from 'react';
 function IncDec() {
   const [p, setP] = useState(0);
 
-  const handleClick = (value) => {
-    setP(p + value);
+  const handleClick = (value, op) => {
+    if (op === "inc")
+      setP(p + value);
+    else if (op === "dec")
+      setP(p - value);
   };
 
   return (
     <div>
-      <p>Value of p: {p}</p>
-      <button onClick={() => handleClick(5)}>+5</button>
-      <button onClick={() => handleClick(10)}>+10</button>
-      <button onClick={() => handleClick(15)}>+15</button>
-      <button onClick={() => handleClick(20)}>+20</button>
-      <button onClick={() => handleClick(-5)}>-5</button>
-      <button onClick={() => handleClick(-10)}>-10</button>
-      <button onClick={() => handleClick(-15)}>-15</button>
-      <button onClick={() => handleClick(-20)}>-20</button>
+      <h1>Value of p: {p}</h1>
+      {/* for increment */}
+      <button className='inc' onClick={() => handleClick(5, "inc")}>5</button>
+      <button className='inc' onClick={() => handleClick(10, "inc")}>10</button>
+      <button className='inc' onClick={() => handleClick(15, "inc")}>15</button>
+      <button className='inc' onClick={() => handleClick(20, "inc")}>20</button>
+      {/* for decrement */}
+      <button className='dec' onClick={() => handleClick(5, "dec")}>5</button>
+      <button className='dec' onClick={() => handleClick(10, "dec")}>10</button>
+      <button className='dec' onClick={() => handleClick(15, "dec")}>15</button>
+      <button className='dec' onClick={() => handleClick(20, "dec")}>20</button>
+
     </div>
   );
 }
